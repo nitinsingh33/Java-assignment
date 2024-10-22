9adf0d7c68c0e77714ea4fc2a473ef7e6998f64e
nitinsingh33 <nitinsingh.iitp@gmail.com>
2024-10-22 23:59:30 +0530

    new

---
+++ Control_statements/For.java
@@ -0,0 +1,10 @@
+package Control_statements;
+
+public class For {
+    public static void main(String[] args) {  
+        //Code of Java for loop  
+        for(int i=1;i<=10;i++){  
+            System.out.println(i);  
+        }  
+    }   
+}
