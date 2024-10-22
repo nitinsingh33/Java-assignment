76696484f326e39f3a838cd0d8a551d2a10f10ab
nitinsingh33 <nitinsingh.iitp@gmail.com>
2024-10-22 23:25:08 +0530

    .

--- Fundamentals/Control_statements/While.java
+++ Fundamentals/Control_statements/While.java
@@ -4,6 +4,7 @@ public class While {
     public static void main(String[] args) {
         int i=1;
         while(i<=10){
+            
             System.out.println(i);
             i++;
         }
