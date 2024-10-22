b776502effefd0a6e3355d571e92da77d5a5680f
nitinsingh33 <nitinsingh.iitp@gmail.com>
2024-10-22 23:31:01 +0530

    .

---
+++ Fundamentals/Control_statements/For.java
@@ -0,0 +1,11 @@
+package Fundamentals.Control_statements;
+
+public class For {
+    public static void main (String[] args) {
+        //code of java for loop
+        for(int i=1; i<10; i++) {
+            System.out.println(i);
+        } 
+    }
+    
+}
